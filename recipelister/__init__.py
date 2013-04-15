62894be808d2186959cc5d4af7d3d9be55fa35a4
Kyle Marsh <kyle.marsh@dreamhost.com>
2013-04-15 16:20:51 -0700

    Changes to help debug unicode problems

--- recipelister/__init__.py
+++ recipelister/__init__.py
@@ -21,30 +21,45 @@ def init_db(bootstrap=app.debug):
         with app.open_resource('bootstrapping/labels.csv') as f:
             csvreader = csv.reader(f, delimiter=';', quotechar='"')
             cols = csvreader.next()  # First line is column names
+            print "Loading Labels"
             for record in csvreader:
-                label = Label(**{x[0]: x[1] for x in zip(cols, record)})
-                db.session.add(label)
+                d = {x[0]: x[1] for x in zip(cols, record)}
+                if not Label.query.filter_by(label=d['label']).first():
+                    label = Label(**d)
+                    db.session.add(label)
+
+        print "Committing labels."
+        db.session.commit()
 
         # Bootstrap Recipes
         with app.open_resource('bootstrapping/recipes.csv') as f:
             csvreader = csv.reader(f, delimiter=';', quotechar='"')
             cols = csvreader.next()  # First line is column names
-            for record in csvreader:
-                recipe = Recipe(**{x[0]: x[1] for x in zip(cols, record)})
-                db.session.add(recipe)
-
-        db.session.commit()
-
-        # Label Recipes
-        with app.open_resource('bootstrapping/recipe-label.csv') as f:
-            csvreader = csv.reader(f, delimiter=';', quotechar='"')
-            cols = csvreader.next()  # First line is column names
+            print "Loading Recipes:"
             for record in csvreader:
                 d = {x[0]: x[1] for x in zip(cols, record)}
-                recipe = Recipe.query.filter_by(id=d['recipe_id']).first()
-                recipe.labels.append(d['label_id'])
+                #d = {x[0]: x[1].decode('utf-8') for x in zip(cols, record)}
+                print "\t%s: %s" % (d['id'], d['title'])
+                recipe = Recipe(**d)
+                db.session.add(recipe)
 
+        print "Committing recipes."
+        #import pdb
+        #pdb.set_trace()
         db.session.commit()
+
+        ## Label Recipes
+        #with app.open_resource('bootstrapping/recipe-label.csv') as f:
+            #csvreader = csv.reader(f, delimiter=';', quotechar='"')
+            #cols = csvreader.next()  # First line is column names
+            #print "Labeling Recipes:"
+            #for record in csvreader:
+                #print '\t%s' % record
+                #d = {x[0]: x[1] for x in zip(cols, record)}
+                #recipe = Recipe.query.filter_by(id=d['recipe_id']).first()
+                #recipe.labels.append(d['label_id'])
+
+        #db.session.commit()
 
 import recipelister.models
 import recipelister.views
