5ed031a04911d474f43740ce257448ff2d2f870f
Kyle Marsh <kyle.marsh@dreamhost.com>
2013-04-16 19:28:46 -0700

    Individual recipe view
    /recipe/<recipe_id> will show you (reasonably pretty) the recipe you give
    it including time required and the labels tied to the recipe

--- recipelister/views.py
+++ recipelister/views.py
@@ -11,6 +11,14 @@ def index():
     return render_template('index.html', recipes=recipes)
 
 
+@app.route("/recipe/<recipe_id>")
+def view_recipe(recipe_id):
+    recipe = Recipe.query.get(recipe_id)
+    if recipe is None:
+        return redirect(url_for('index'))
+    return render_template('recipe.html', recipe=recipe)
+
+
 @app.errorhandler(404)
 def page_not_found(error):
     return render_template('page_not_found.html'), 404
