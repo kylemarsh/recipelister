053b5e836b03eeebc021b75894ae7731831cb562
Kyle Marsh <kyle.l.marsh+github@gmail.com>
2022-09-21 06:52:58 -0700

    split tagstuff into components; move into own file

--- react_frontend/src/Recipe.js
+++ react_frontend/src/Recipe.js
@@ -1,5 +1,6 @@
 import React from "react";
 import NoteList from "./Notes";
+import TagList from "./Tags";
 import * as Util from "./Util";
 
 const Recipe = (props) => {
@@ -12,30 +13,13 @@ const Recipe = (props) => {
       <h2>{recipe.Title}</h2>
       <p className="recipe-body">{recipe.Body}</p>
       <span className="tag-list-title">Tags</span>
-      <div className="tag-list-container">
-        <TagList
-          tags={recipe.Labels}
-          handleUnlinkClick={props.handleLabelUnlinkClick}
-        />
-        <span className="link-tag-trigger" onClick={props.handleLabelLinkClick}>
-          &oplus;
-        </span>
-        <form
-          className="link-tag-form hidden"
-          onSubmit={props.handleLabelLinkSubmit}
-        >
-          <input name="label" type="text" placeholder="label" />
-          <button className="inline-text-button submit">
-            {String.fromCharCode(0x2713)}
-          </button>
-          <button
-            className="inline-text-button cancel"
-            onClick={props.handleLabelLinkCancel}
-          >
-            {String.fromCharCode(0x2717)}
-          </button>
-        </form>
-      </div>
+      <TagList
+        tags={recipe.Labels}
+        handleUnlinkClick={props.handleLabelUnlinkClick}
+        handleLabelLinkClick={props.handleLabelLinkClick}
+        handleLabelLinkSubmit={props.handleLabelLinkSubmit}
+        handleLabelLinkCancel={props.handleLabelLinkCancel}
+      />
       <span className="note-list-title">Notes</span>
       <NoteList
         notes={recipe.Notes}
@@ -50,28 +34,6 @@ const Recipe = (props) => {
       />
     </div>
   );
-};
-
-const TagList = (props) => {
-  if (!props.tags || !props.tags.length) {
-    return <div>no tags</div>;
-  }
-  const tags = props.tags.map((tag) => {
-    return (
-      <li key={tag.ID} data-tag-id={tag.ID} data-tag-name={tag.Label}>
-        {tag.Label}
-        <span
-          className="tag-unlink"
-          role="img"
-          aria-label="close-icon"
-          onClick={props.handleUnlinkClick}
-        >
-          &otimes;
-        </span>
-      </li>
-    );
-  });
-  return <ul className="tag-list">{tags}</ul>;
 };
 
 export default Recipe;
---
+++ react_frontend/src/Tags.js
@@ -0,0 +1,70 @@
+import React from "react";
+
+const TagList = (props) => {
+  var tags;
+  if (props.tags && props.tags.length) {
+    tags = props.tags.map((tag) => {
+      return (
+        <TagListItem
+          key={tag.ID}
+          tag={tag}
+          unlinkHandler={props.handleUnlinkClick}
+        />
+      );
+    });
+  }
+  return (
+    <div className="tag-list-container">
+      <ul className="tag-list">
+        {tags}
+        <li>
+          <LinkTagForm
+            handleTriggerClick={props.handleLabelLinkClick}
+            handleSubmit={props.handleLabelLinkSubmit}
+            handleCancel={props.handleLabelLinkCancel}
+          />
+        </li>
+      </ul>
+    </div>
+  );
+};
+
+const TagListItem = (props) => {
+  return (
+    <li data-tag-id={props.tag.ID} data-tag-name={props.tag.Label}>
+      {props.tag.Label}
+      <span
+        className="tag-unlink"
+        role="img"
+        aria-label="delete-icon"
+        onClick={props.unlinkHandler}
+      >
+        &otimes;
+      </span>
+    </li>
+  );
+};
+
+const LinkTagForm = (props) => {
+  return (
+    <div className="link-tag-container">
+      <span className="link-tag-trigger" onClick={props.handleTriggerClick}>
+        + add label
+      </span>
+      <form className="link-tag-form hidden" onSubmit={props.handleSubmit}>
+        <input name="label" type="text" placeholder="label" />
+        <button className="inline-text-button submit">
+          {String.fromCharCode(0x2713)}
+        </button>
+        <button
+          className="inline-text-button cancel"
+          onClick={props.handleCancel}
+        >
+          {String.fromCharCode(0x2717)}
+        </button>
+      </form>
+    </div>
+  );
+};
+
+export default TagList;
