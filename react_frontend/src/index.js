4ee8d424017616ddbb36fae591b954307fdda235
Kyle Marsh <kyle.l.marsh+github@gmail.com>
2022-09-24 21:57:33 -0700

    upgrade from ReactDOM.render

--- react_frontend/src/index.js
+++ react_frontend/src/index.js
@@ -1,5 +1,5 @@
 import React from "react";
-import ReactDOM from "react-dom";
+import { createRoot } from "react-dom/client";
 import App from "./App";
 import "react-widgets/styles.css";
 import "./index.css";
@@ -11,12 +11,13 @@ import "./Tags.css";
 import "./ResultList.css";
 import * as serviceWorker from "./serviceWorker";
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById("root")
-);
+const root = createRoot(document.getElementById("root"));
+//root.render(
+//<React.StrictMode>
+//<App />
+//</React.StrictMode>
+//);
+root.render(<App />);
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
